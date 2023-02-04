c2839d13b0442528d9fe9217b673fe13e59c446f
Diaby Almamy <diabyissouf149@gmail.com>
2023-02-04 16:55:25 +0000

    model utilisateur créé et ajouté à l'interface d'admin.

---
+++ src/account/models.py
@@ -0,0 +1,73 @@
+from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.utils import timezone
+
+
+class CustomUserManager(BaseUserManager):
+
+    def _create_user(self, username, password, **extra_fields):
+        """
+        Create and save a user with the given username and password.
+        """
+        if not username:
+            raise ValueError('Veuillez définir un username.')
+
+        username = self.model.normalize_username(username)
+        user = self.model(username=username, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_user(self, username, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(username, password, **extra_fields)
+
+    def create_superuser(self, username, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_admin', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_admin') is not True:
+            raise ValueError('Superuser must have is_admin=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self._create_user(username, password, **extra_fields)
+
+
+class CustomUser(AbstractBaseUser):
+    username = models.CharField(
+        max_length=150,
+        unique=True,
+        help_text='Required. 150 characters or fewer. Letters, digits.',
+        blank=False,
+    )
+    joindate = models.DateTimeField(default=timezone.now)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_admin = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+
+    USERNAME_FIELD = "username"
+
+    objects = CustomUserManager()
+
+    def has_perm(self, perm, obj=None):
+        return True
+
+    def has_module_perms(self, app_label):
+        return True
+
+    def save(self, *args, **kwargs):
+        if not self.username:
+            raise ValueError("Veuillez définir un nom d'utilisateur (username)")
+        if not self.password:
+            raise ValueError("Veuillez définir un mot de passe (password)")
+
+        super().save(*args, **kwargs)
+
+
