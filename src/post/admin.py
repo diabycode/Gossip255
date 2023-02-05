8a359854714c9f95109ebce32fe636fc15599eb4
Diaby Almamy <diabyissouf149@gmail.com>
2023-02-05 17:45:32 +0000

    model Post créé.

--- src/post/admin.py
+++ src/post/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Post, HashTag
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+
+    list_display = ("content", "author")
+
+
+@admin.register(HashTag)
+class HashTagAdmin(admin.ModelAdmin):
+    list_display = ("name", )
+
--- src/post/models.py
+++ src/post/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+from account.models import CustomUser
+
+
+class HashTag(models.Model):
+    name = models.CharField(max_length=50, blank=False)
+
+
+class Post(models.Model):
+    author = models.ForeignKey(CustomUser, on_delete=models.CASCADE, blank=False)
+    content = models.TextField(blank=False)
+    thumbnail = models.ImageField(blank=True, null=True)
+    hashtags = models.ManyToManyField(HashTag, blank=True)
+
+    def save(self, force_insert=False, force_update=False, using=None,
+             update_fields=None):
+
+        for word in self.content.split(" "):
+            if word.startswith("#"):
+                hashtag = HashTag.objects.create(name=word[1:])
+
+                self.hashtags.set([hashtag])
+
+        super().save(force_insert=False, force_update=False, using=None, update_fields=None)
+
